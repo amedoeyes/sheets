8fe942b72d6ccfc018891268cb71bf7042f78ac3
amedoeyes <amedoeyes@gmail.com>
2023-01-27 22:57:02 +0200

    pwa

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ export default function App() {
 					/>
 				}
 			/>
-			{/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+			<Route path="*" element={<Navigate to="/" replace />} />
 		</Routes>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			manifest: {
+				name: "Sheets",
+				short_name: "Sheets",
+				description: "Survey sheets",
+				orientation: "portrait-primary",
+				theme_color: "#101010",
+				background_color: "#101010",
+				icons: [
+					{
+						src: "icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+			devOptions: {
+				enabled: process.env.NODE_ENV === "development" ? true : false,
+			},
+		}),
+	],
+});
