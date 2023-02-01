6293da077e8a887068af73d1e225c0456e9f8109
amedoeyes <amedoeyes@gmail.com>
2023-02-01 08:28:56 +0200

    auto updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-console.log(process.env.RELOAD_SW);
-
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
@@ -29,6 +26,7 @@ export default defineConfig({
 					},
 				],
 			},
+			registerType: "autoUpdate",
 			devOptions: {
 				enabled: process.env.NODE_ENV === "development",
 			},
